0eb3e481c705cd3db5417280bb1df7f0a1e21b82
kintalisriharsha <ksriharsha669@gmail.com>
2025-08-06 14:01:20 +0530

    commit

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 app = FastAPI()
 
@@ -8,6 +12,10 @@ app = FastAPI()
 def read_root():
     index_path = os.path.join(os.path.dirname(__file__), "static/index.html")
     return FileResponse(index_path, media_type="text/html")
+
+@app.get("/db_url")
+def get_db_url():
+    return {"DATABASE_URL": DATABASE_URL}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
